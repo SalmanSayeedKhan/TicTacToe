0c219ecdd345110ee73b38515c1994d0101fad84
Salman Sayeed <salmansayeedkhan@gmail.com>
2020-12-24 23:02:19 +0600

    Diagonal Issue fixed.

--- TicTacToe.java
+++ TicTacToe.java
@@ -67,7 +67,7 @@ public class TicTacToe {
     public static boolean checkBoard(char[][] arr)
     {
         return (arr[0][0] == arr[0][1] && arr[0][0] == arr[0][2]) || (arr[1][0] == arr[1][1] && arr[1][0] == arr[1][2])|| (arr[2][0] == arr[2][1] && arr[2][0] == arr[2][2])||
-                (arr[0][0] == arr[1][0] && arr[0][0] == arr[2][0]) || (arr[0][1] == arr[1][1] && arr[0][1] == arr[2][1]) || (arr[0][2] == arr[1][2] && arr[0][2] == arr[2][2]);
+                (arr[0][0] == arr[1][0] && arr[0][0] == arr[2][0]) || (arr[0][1] == arr[1][1] && arr[0][1] == arr[2][1]) || (arr[0][2] == arr[1][2] && arr[0][2] == arr[2][2]) || (arr[0][0]==arr[1][1] && arr[1][1] == arr[2][2]) || (arr[0][2]==arr[1][1] && arr[1][1]==arr[2][0]);
     }
     public static void main(String []args)
     {
